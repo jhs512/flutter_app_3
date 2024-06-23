90a022b9983a774068463d80309a7960f93d2940
jhs512 <jangka512@gmail.com>
2024-06-23 23:53:31 +0900

    work

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.freezed.dart';
+
+part 'main.g.dart';
+
+@riverpod
+class CountState extends _$CountState {
+  @override
+  int build() => 0;
+
+  void increment() {
+    state++;
+  }
+}
+
+@freezed
+class Todo with _$Todo {
+  const factory Todo({
+    required String title,
+    required String description,
+  }) = _Todo;
+
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
+}
+
+@riverpod
+class TodosState extends _$TodosState {
+  @override
+  List<Todo> build() => [
+        Todo(
+          title: 'title',
+          description: 'description',
+        ),
+        Todo(
+          title: 'title',
+          description: 'description',
+        )
+      ];
+
+  void add(Todo todo) {
+    state = [...state, todo];
+  }
+}
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,60 +62,65 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyHomePage extends HookConsumerWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todosState = ref.watch(todosStateProvider);
+    final countStateValue = ref.watch(countStateProvider);
+    final countState = ref.read(countStateProvider.notifier);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    final count2State = useState(0);
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+    return GestureDetector(
+      onTap: () => {
+        countState.increment(),
+        count2State.value++,
+      },
+      child: Scaffold(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text('You have pushed the button this many times:'),
+            Text(
+              '${countStateValue}',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             Text(
-              '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headlineMedium,
+              '${count2State.value}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(todosStateProvider.notifier).add(
+                      Todo(
+                        title: 'title',
+                        description: 'description',
+                      ),
+                    );
+              },
+              child: const Text('Add Todo'),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: todosState.length,
+                itemBuilder: (context, index) {
+                  final todo = todosState[index];
+                  return ListTile(
+                    title: Text(todo.title),
+                    subtitle: Text(todo.description),
+                  );
+                },
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
